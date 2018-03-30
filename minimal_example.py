c4bee1dfb10a50aa1f0046d93bc8048a8f35fd1c
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2018-03-30 10:14:42 -0500

    minimal example from new source

--- minimal_example.py
+++ minimal_example.py
@@ -1,117 +1,82 @@
-import time
+import cv2
+from time import sleep
 import numpy as np
-import picamera
-from threading import Thread
-import logging
+import picam
+import RPi.GPIO as GPIO #RPi GPIO controller
 
-class BaseSensor(object):
+if __name__ == '__main__':
+	try:
+		camera = picam.OpenCVCapture()
+		ret, image = camera.read()
+		image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+		thresh = 127
+		maxValue = 255
 
-    def start(self):
-        '''
-        Start receiving values from the sensor
-        '''
-        t = Thread(target=self.update, args=())
-        t.daemon = True
-        t.start()
-        time.sleep(1)
-        return self
+		if ret:
+				# The frame is ready and already captured
+				#cv2.imshow('frame', gray)
 
-    def update(self):
-        '''
-        Performs sensor update steps. This is called
-        in a separate thread
-        '''
-        pass
+				#This is the meat.  It processes the grayscale'd frame for contours based on the threshold info.
+				th, dst = cv2.threshold(image, thresh, maxValue, cv2.THRESH_BINARY)
+				contours, hierarchy = cv2.findContours(dst, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+				dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+				#Then we draw the contour on the color original
+				cv2.drawContours(frame, contours, -1, (255, 255, 0), 3)
 
-class BaseCamera(BaseSensor):
+				#Considers the first contour detected in a frame.
+				cnt = contours[0]
 
-    def __init__(self, resolution=(640,480), **kwargs):
-        super(BaseCamera, self).__init__(**kwargs)
+				#Determines the moments of the contoured shape in the frame, and their XY coordinate
+				M = cv2.moments(cnt)
+				cx = int(M['m10']/M['m00'])
+				cy = int(M['m01']/M['m00'])
 
-        self.resolution = resolution
-        self.frame = np.zeros(
-            shape=(
-                self.resolution[1],
-                self.resolution[0],
-                3))
-        self.frame_time = 0
-        self.encoded_time = 0
-        self.base64_time = 0
+				height, width, channels = frame.shape
+				whalf = width/2
+				hhalf = height/2
 
-    def image_buffer(self):
-        '''
-        Returns the JPEG image buffer corresponding to
-        the current frame. Caches result for
-        efficiency.
-        '''
-        if self.frame_time > self.encoded_time:
-            arr = self.frame
-            img = Image.fromarray(arr, 'RGB')
-            encoded_buffer = cStringIO.StringIO()
-            img.save(encoded_buffer, format="JPEG",
-                     quality=100, subsampling=0)
-            self.encoded_buffer = encoded_buffer
-            self.encoded_time = time.time()
-        else:
-            encoded_buffer = self.encoded_buffer
-        return encoded_buffer
+				#Check which how motors need to move to put moon in center
+				if cx < whalf:
+					print "pan camera RIGHT"
+					sleep(0.05)
+					GPIO.output(13, GPIO.LOW)
+				elif cx > whalf:
+					print "pan camera LEFT"
+					GPIO.output(15, GPIO.HIGH)
+					sleep(0.05)
+					GPIO.output(15, GPIO.LOW)
+				else:
+					print "Center X"
+				if cy < hhalf:
+					print "pan camera DOWN"
+					GPIO.output(16, GPIO.HIGH)
+					sleep(0.05)
+					GPIO.output(16, GPIO.LOW)
+				elif cy > hhalf:
+					print "pan camera UP"
+					GPIO.output(18, GPIO.HIGH)
+					sleep(0.05)
+					GPIO.output(18, GPIO.LOW)
+				else:
+					print "Center Y"
+				#cv2.imshow("Contour",frame)
+			else:
+				# The next frame is not ready, so we try to read it again
+				image.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+				print "frame is not ready"
+				# It is better to wait for a while for the next frame to be ready
+				cv2.waitKey(1000)
 
-    def base64(self):
-        '''
-        Returns a base-64 encoded string corresponding
-        to the current frame. Caches result for
-        efficiency.
-        '''
-        if self.frame_time > self.base64_time:
-            image_buffer = self.image_buffer()
-            image_buffer.seek(0)
-            base64_buffer = base64.b64encode(image_buffer.read())
-            self.base64_buffer = base64_buffer
-            self.base64_time = time.time()
-        else:
-            base64_buffer = self.base64_buffer
-        return base64_buffer
-
-class PiVideoStream(BaseCamera):
-
-    def __init__(self, resolution=(640,480),
-                 framerate=32,
-                 rotation=0, **kwargs):
-        from picamera.array import PiRGBArray
-        from picamera import PiCamera
-
-        super(PiVideoStream, self).__init__(resolution, **kwargs)
-
-        self.camera = PiCamera()
-        self.camera.resolution = resolution
-        self.camera.framerate = framerate
-        self.camera.rotation = rotation
-        self.camera.exposure_mode = "sports"
-        self.rawCapture = PiRGBArray(self.camera, size=resolution)
-
-        self.frame = None
-        self.stopped = False
-
-        logging.info("PiVideoStream loaded.. .warming camera")
-
-        time.sleep(2)
-        self.start()
-
-    def update(self):
-        for f in self.camera.capture_continuous(
-                self.rawCapture, format="rgb", use_video_port=True):
-            self.frame = f.array
-
-            self.rawCapture.truncate(0)
-            self.frame_time = time.time()
-
-            if self.stopped:
-                self.stream.close()
-                self.rawCapture.close()
-                self.camera.close()
-                return
-
-    def stop(self):
-        self.stopped = True
-
-BaseCamera()
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+except KeyboardInterrupt:
+	print 'exiting'
+except picamera.PiCameraMMALError:
+	print 'MMAL Error Occurred, but the program will be shutting down correctly'
+except:
+	print 'An Unknown Error Occurred.  Helpful, right?'
+finally:
+#	image.release()
+	RAWCAPTURE.truncate(0)
+	GPIO.cleanup()
+	cv2.destroyAllWindows()
