fd7ae728ed41948998cdb495b195700343a880a7
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2018-04-06 15:21:29 -0500

    Pythonic simpletracker

--- simple_tracker.py
+++ simple_tracker.py
@@ -16,124 +16,70 @@ import io
 import time
 import subprocess
 
+# Package imports requiring pip installs
 import numpy as np
 import cv2
 import RPi.GPIO as GPIO #RPi GPIO controller
 import picamera
 
+# Tell Windows users to GTFO
+if os.name == 'nt':
+	print "Some parts of this program may be incompatible with Windows"
+
 # Be sure we have access to GPIOmem
-if "root gpio" not in (subprocess.check_output(['ls', '-l', '/dev/gpiomem'])):
+if "root gpio" not in subprocess.check_output(['ls', '-l', '/dev/gpiomem']):
 	subprocess.call(['sudo', 'chmod', 'g+rw' '/dev/gpiomem'])
 	subprocess.call(['sudo', 'chown', 'root.gpio' '/dev/gpiomem'])
 
-# Setup GPIO
+# Defines the pins being used for the GPIO pins.
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT) # A PWM
-GPIO.setup(12, GPIO.OUT) # A input 2
-GPIO.setup(13, GPIO.OUT) # A input 1
-GPIO.setup(15, GPIO.OUT) # B input 1
-GPIO.setup(16, GPIO.OUT) # B input 2
-GPIO.setup(18, GPIO.OUT) # B PWM
+XPIN1 = 15
+XPIN2 = 16
+XPINP = 18
+YPIN1 = 13
+YPIN2 = 12
+YPINP = 11
+PINS = (XPIN1, XPIN2, XPINP, YPIN1, YPIN2, YPINP)
 
-# Start GPIO with 'off' values
-GPIO.output(11, GPIO.HIGH)
-GPIO.output(12, GPIO.LOW)
-GPIO.output(13, GPIO.LOW)
-GPIO.output(15, GPIO.LOW)
-GPIO.output(16, GPIO.LOW)
-GPIO.output(18, GPIO.HIGH)
+# Setup GPIO and start them with 'off' values
+for i in PINS:
+	GPIO.setup(PINS[i], GPIO.OUT)
+	if i != 0 or 5:
+		GPIO.output(PINS[i], GPIO.LOW)
+	else:
+		GPIO.output(PINS[i], GPIO.HIGH)
 
-
+# Sets up an output file labeled with the current unixtime
 OUTFILE = int(time.time())
 OUTFILE = str(OUTFILE) + 'out.mp4'
 subprocess.call(['touch', str(OUTFILE)])
 
-if __name__ == '__main__':
+def main():
+	'''This is the main chunk of the program
+	'''
 	try:
 		while True:
 
-			DATA = io.BytesIO()
+			data = io.BytesIO()
 			with picamera.PiCamera() as picam:
 				#picam.resolution = (1920, 1080)
-				picam.capture(DATA, format='jpeg', use_video_port=False, resize=(1920, 1080))
-			DATA = np.fromstring(DATA.getvalue(), dtype=np.uint8)
-			IMAGE = cv2.imdecode(DATA, 1)
-			cv2.imwrite('debugimage.jpg', IMAGE)
-	#		CAMERA = picam.OpenCVCapture()
-	#		RET, IMAGE = cv2.imread()
-			IMAGE = cv2.cvtColor(IMAGE, cv2.COLOR_RGB2GRAY)
-			THRESH = 127
-			MAX_VALUE = 255
+				picam.capture(data, format='jpeg', use_video_port=False, resize=(1920, 1080))
+			data = np.fromstring(data.getvalue(), dtype=np.uint8)
+			image = cv2.imdecode(data, 1)
+			cv2.imwrite('debugimage.jpg', image)
+			image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+			thresh = 127
+			max_value = 255
 
-			#This is the meat.  It processes the grayscale'd frame for contours based on the THRESHold info.
-			TH, DST = cv2.threshold(IMAGE, THRESH, MAX_VALUE, cv2.THRESH_BINARY)
-			CONTOURS, HIERARCHY = cv2.findContours(DST, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-			DST = cv2.cvtColor(DST, cv2.COLOR_GRAY2BGR)
+			#This is the meat.  It processes the grayscale'd frame for contours based on the threshold info.
+			th, dst = cv2.threshold(image, thresh, max_value, cv2.thresh_BINARY)
+			contours, hierarchy = cv2.findContours(dst, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+			dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
 
 			#Then we draw the contour on the color original
-			cv2.drawContours(IMAGE, CONTOURS, -1, (255, 255, 0), 3)
+			cv2.drawContours(image, contours, -1, (255, 255, 0), 3)
 
-			#Considers the first contour detected in a frame.
-			CNT = CONTOURS[0]
-
-			#Determines the moments of the contoured shape in the frame, and their XY coordinate
-			M = cv2.moments(CNT)
-			if int(M['m00']) != 0:
-				CX = int(M['m10']/M['m00'])
-				CY = int(M['m01']/M['m00'])
-
-				HEIGHT, WIDTH = IMAGE.shape
-				WIDTH = WIDTH/2
-				HEIGHT = HEIGHT/2
-
-				#Check which how motors need to move to put moon in center
-				if CX < WIDTH:
-					print "pan camera RIGHT"
-					GPIO.output(15, GPIO.LOW) #1
-					GPIO.output(16, GPIO.HIGH) #2
-					GPIO.output(18, GPIO.HIGH) #pwm
-					time.sleep(0.05)
-					#GPIO.output(18, GPIO.LOW)
-				elif CX > WIDTH:
-					print "pan camera LEFT"
-					GPIO.output(15, GPIO.HIGH) #1
-					GPIO.output(16, GPIO.LOW) #2
-					GPIO.output(18, GPIO.HIGH) #pwm
-					time.sleep(0.05)
-					#GPIO.output(18, GPIO.LOW)
-				else:
-					print "Center X"
-					GPIO.output(15, GPIO.LOW) #1
-					GPIO.output(16, GPIO.LOW) #2
-					GPIO.output(18, GPIO.HIGH) #pwm
-				if CY < HEIGHT:
-					print "pan camera DOWN"
-					GPIO.output(13, GPIO.LOW) #1
-					GPIO.output(12, GPIO.HIGH) #2
-					GPIO.output(11, GPIO.HIGH) #pwm
-					time.sleep(0.05)
-					#GPIO.output(16, GPIO.LOW)
-				elif CY > HEIGHT:
-					print "pan camera UP"
-					GPIO.output(13, GPIO.HIGH) #1
-					GPIO.output(12, GPIO.LOW) #2
-					GPIO.output(11, GPIO.HIGH) #pwm
-					time.sleep(0.05)
-					#GPIO.output(18, GPIO.LOW)
-				else:
-					print "Center Y"
-					GPIO.output(13, GPIO.LOW) #1
-					GPIO.output(12, GPIO.LOW) #2
-					GPIO.output(11, GPIO.HIGH) #pwm
-
-			if os.path.isfile(OUTFILE) is True:
-				subprocess.call(['ffmpeg', '-loglevel', 'quiet', '-y', '-i', 'debugimage.jpg', '-c:v',
-						'libx264', '-filter_complex', '[0] concat=n=1:v=1:a=0 [v]', '-map', '[v]',
-						str(OUTFILE)], stdout=open(os.devnull, 'w'))
-			else:
-				subprocess.call(['ffmpeg', '-loglevel', 'quiet', 'y', '-i', str(OUTFILE), '-i', 'debugimage.jpg',
-						'-c:v', 'libx264', '-filter_complex', '"[0][1] concat=n=2:v=1:a=0 [v]"',
-						'-map', '"[v]"', str(OUTFILE)], stdout=open(os.devnull, 'w'))
+			gpio(contours, image)
 
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				print 'done'
@@ -148,3 +94,71 @@ if __name__ == '__main__':
 	finally:
 		GPIO.cleanup()
 		cv2.destroyAllWindows()
+
+def gpio(contours, image):
+	'''This section controls the GPIO pin movement
+	'''
+
+	#Considers the first contour detected in a frame.
+	contours = contours[0]
+
+	#Determines the moments of the contoured shape in the frame, and their XY coordinate
+	m = cv2.moments(contours)
+
+	#The following if prevents and ignores DivideByZero exceptions.
+	#This is a quick and dirty fix which is not meant to see the light of day.
+	if int(m['m00']) != 0:
+		cx = int(m['m10']/m['m00'])
+		cy = int(m['m01']/m['m00'])
+
+		height, width = image.shape
+		width = width/2
+		height = height/2
+
+		#Check which how motors need to move to put moon in center
+		if cx < width:
+			print "pan camera RIGHT"
+			movex = True
+			state = (GPIO.LOW, GPIO.HIGH, GPIO.HIGH)
+			#GPIO.output(18, GPIO.LOW)
+		elif cx > width:
+			print "pan camera LEFT"
+			movex = True
+			state = (GPIO.HIGH, GPIO.LOW, GPIO.HIGH)
+		else:
+			print "Center X"
+			movex = True
+			state = (GPIO.LOW, GPIO.LOW, GPIO.HIGH)
+		motor(movex, state)
+
+		if cy < height:
+			print "pan camera DOWN"
+			movex = False
+			state = (GPIO.LOW, GPIO.HIGH, GPIO.HIGH)
+		elif cy > height:
+			print "pan camera UP"
+			movex = False
+			state = (GPIO.HIGH, GPIO.LOW, GPIO.HIGH)
+		else:
+			print "Center Y"
+			movex = False
+			state = (GPIO.LOW, GPIO.LOW, GPIO.HIGH)
+		motor(movex, state)
+
+def motor(movex, state):
+	'''This sends the information to the gearmotors.
+	Programming is designed for TB6612FNG
+	'''
+	if movex:
+		GPIO.output(XPIN1, state[0])
+		GPIO.output(XPIN2, state[1])
+		GPIO.output(XPINP, state[2])
+	if not movex:
+		GPIO.output(YPIN1, state[0])
+		GPIO.output(YPIN2, state[1])
+		GPIO.output(YPINP, state[2])
+	time.sleep(0.05)
+
+
+if __name__ == '__main__':
+	main()
