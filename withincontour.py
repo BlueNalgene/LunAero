2986180ad470936fdb63397b243db7e01dd7f765
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2018-04-10 13:56:08 -0500

    withincontour pylint spacing corrections

--- withincontour.py
+++ withincontour.py
@@ -14,13 +14,13 @@ cap = cv2.VideoCapture('Migrants.mp4')
 #cap = cv2.VideoCapture('Nocturnal.mp4')
 
 #This is the background removing step
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(100,7,0.5,5)
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(100, 7, 0.5, 5)
 
 #This defines a matrix for other functions
-mat = np.ones((3,3),np.uint8)
+mat = np.ones((3, 3), np.uint8)
 
 try:
-	while(cap.isOpened()):
+	while cap.isOpened():
 		ret, frame = cap.read()
 
 		#Defines some important things
@@ -35,14 +35,14 @@ try:
 			#This is where any image manipulation happens
 			blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 			can = cv2.Canny(blurred, 5, 200)
-			fgmask = cv2.addWeighted(fgmask,0.5,can,0.5,0)
+			fgmask = cv2.addWeighted(fgmask, 0.5, can, 0.5, 0)
 			fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_GRADIENT, mat)
 
 			#ret,thresh = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
 			#contours,hierarchy = cv2.findContours(thresh, 3, 2)
 			#nocolor = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
-			ret,thresh = cv2.threshold(gray,40,255,cv2.THRESH_BINARY)
-			_,contours,hierarchy = cv2.findContours(thresh, 2, 1)
+			ret, thresh = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY)
+			_, contours, hierarchy = cv2.findContours(thresh, 2, 1)
 
 			# We only care about the BIGGEST contour here
 			c = max(contours, key = cv2.contourArea)
@@ -53,14 +53,14 @@ try:
 			mask = np.zeros(frame.shape, dtype=np.uint8)
 			# This draws the ellipse onto the empty shape we just made.
 			# The parameters are from the fitEllipse, but they have to be INT.
-			cv2.ellipse(mask,(int(ellipse[0][0]),int(ellipse[0][1])),(int(ellipse[1][0]),int(ellipse[1][1])),int(ellipse[2]),0,360,(255, 255, 255),-1,8)
+			cv2.ellipse(mask, (int(ellipse[0][0]), int(ellipse[0][1])), (int(ellipse[1][0]), int(ellipse[1][1])), int(ellipse[2]), 0, 360, (255, 255, 255), -1, 8)
 			result = frame & mask
 
 			# Now the mask is shifted such that the center of the ellipse is in the center of the screen.
 			xdi = (col / 2) - int(ellipse[0][0])
 			ydi = (row / 2) - int(ellipse[0][1])
-			M = np.float32([[1,0,xdi],[0,1,ydi]])
-			result = cv2.warpAffine(result, M, (col,row))
+			M = np.float32([[1, 0, xdi], [0, 1, ydi]])
+			result = cv2.warpAffine(result, M, (col, row))
 
 			# Subtract background from stilled frame
 			# learningRate = 0.05 deals with moon features better, but may be too taxing for Pi
@@ -72,8 +72,8 @@ try:
 
 			# Retest the image for contours
 			#gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-			ret,thresh = cv2.threshold(result,127,255,cv2.THRESH_BINARY)
-			_,contours,hierarchy = cv2.findContours(thresh, 2, 1)
+			ret, thresh = cv2.threshold(result, 127, 255, cv2.THRESH_BINARY)
+			_, contours, hierarchy = cv2.findContours(thresh, 2, 1)
 
 			##This if is required to ignore initial frames
 			#if not contours:
@@ -173,16 +173,16 @@ try:
 						####radius = int(radius)
 						####cv2.circle(frame,center,radius,(0,0,255),2)
 
-			cv2.imwrite('current.png',frame)
+			cv2.imwrite('current.png', frame)
 			# This mess displays different things side by side
 			# The Mother window shows the manipulated image that is being used for cakculations
 			# The Original window shows the contours overlaid on the original frame
 			cv2.namedWindow("Mother", cv2.WINDOW_NORMAL)
-			cv2.resizeWindow("Mother",700,500)
-			cv2.imshow("Mother",result)
+			cv2.resizeWindow("Mother", 700, 500)
+			cv2.imshow("Mother", result)
 			cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
-			cv2.resizeWindow("Original",700,500)
-			cv2.imshow("Original",frame)
+			cv2.resizeWindow("Original", 700, 500)
+			cv2.imshow("Original", frame)
 		else:
 			# The next frame is not ready, so we try to read it again
 			cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
