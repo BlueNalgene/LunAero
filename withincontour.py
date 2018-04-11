dcfecd1c279522c548d78dd6352ce6a5d7152258
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2018-04-11 10:19:22 -0500

    pythonic withincontour

--- withincontour.py
+++ withincontour.py
@@ -1,9 +1,15 @@
+#!/bin/usr/python
+
+'''
 # This is a test program for the LunAero project which is able to:
 #  - Locate a simplified moon
 #  - Put a contour around the moon
 #  - Track the motion of the moon from the center of the screen
 
 #This particular file centers the moon and subtracks the background.
+'''
+
+import sys
 
 import numpy as np
 import cv2
@@ -19,61 +25,99 @@ fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(100, 7, 0.5, 5)
 #This defines a matrix for other functions
 mat = np.ones((3, 3), np.uint8)
 
-try:
-	while cap.isOpened():
-		ret, frame = cap.read()
+def main():
+	'''This is the main function of the code.
+	'''
+	try:
+		while cap.isOpened():
+			ret, frame = cap.read()
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		#Defines some important things
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-		# This if loop detects the contours in the manipulated image and displays
 		if ret:
+			cv_contour()
+			cv2.imwrite('current.png', frame)
+			# This mess displays different things side by side
+			# The Mother window shows the manipulated image that is being used for cakculations
+			# The Original window shows the contours overlaid on the original frame
+			cv2.namedWindow("Mother", cv2.WINDOW_NORMAL)
+			cv2.resizeWindow("Mother", 700, 500)
+			cv2.imshow("Mother", result)
+			cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
+			cv2.resizeWindow("Original", 700, 500)
+			cv2.imshow("Original", frame)
+		else:
+			# The next frame is not ready, so we try to read it again
+			cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+			print "frame is not ready"
+			# It is better to wait for a while for the next frame to be ready
+			cv2.waitKey(1000)
 
-			col = len(frame[0])
-			row = len(frame)
+		if cv2.waitKey(10) & 0xFF == ord('q'):
+			return
 
-			#This is where any image manipulation happens
-			blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-			can = cv2.Canny(blurred, 5, 200)
-			fgmask = cv2.addWeighted(fgmask, 0.5, can, 0.5, 0)
-			fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_GRADIENT, mat)
+	except KeyboardInterrupt:
+		print "Exit code issued"
+	except Exception as errorflag:
+		errorflag = sys.exc_info()[0]
+		print '\033[91m'+ "Error: %s" % errorflag + '\033[0m'
+		raise
+	finally:
+		cap.release()
+		cv2.destroyAllWindows()
+
+def cv_manip():
+	'''This function is where image manipulation happens.
+	This is currently unused
+	'''
+	blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+	can = cv2.Canny(blurred, 5, 200)
+	fgmask = cv2.addWeighted(fgmask, 0.5, can, 0.5, 0)
+	fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_GRADIENT, mat)
+
+def cv_contour():
+	'''This function takes care of contour selections
+	'''
+	ret, thresh = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY)
+	_, contours, hierarchy = cv2.findContours(thresh, 2, 1)
+
+	# We only care about the BIGGEST contour here
+	c = max(contours, key = cv2.contourArea)
+
+	# We treat it as an ellipse to account for irregularities in shape.
+	ellipse = cv2.fitEllipse(c)
+
+	# This makes an 'image' of all nothing with the same size as the original
+	mask = np.zeros(frame.shape, dtype=np.uint8)
+	# This draws the ellipse onto the empty shape we just made.
+	# The parameters are from the fitEllipse, but they have to be INT.
+	cv2.ellipse(mask, (int(ellipse[0][0]), int(ellipse[0][1])), (int(ellipse[1][0]), int(ellipse[1][1])), int(ellipse[2]), 0, 360, (255, 255, 255), -1, 8)
+	result = frame & mask
+
+	# Now the mask is shifted such that the center of the ellipse is in the center of the screen.
+	col = len(frame[0])
+	row = len(frame)
+	xdi = (col / 2) - int(ellipse[0][0])
+	ydi = (row / 2) - int(ellipse[0][1])
+	M = np.float32([[1, 0, xdi], [0, 1, ydi]])
+	result = cv2.warpAffine(result, M, (col, row))
+
+	# Subtract background from stilled frame
+	# learningRate = 0.05 deals with moon features better, but may be too taxing for Pi
+	result = fgbg.apply(result, learningRate=.1)
+	#result = cv2.GaussianBlur(result, (3, 3), 0)
+	#can = cv2.Canny(result, 5, 200)
+	#result = cv2.addWeighted(result,0.5,can,0.5,0)
+	result = cv2.morphologyEx(result, cv2.MORPH_GRADIENT, mat)
+
+	# Retest the image for contours
+	#gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+	ret, thresh = cv2.threshold(result, 127, 255, cv2.THRESH_BINARY)
+	_, contours, hierarchy = cv2.findContours(thresh, 2, 1)
+	return result, frame
 
 			#ret,thresh = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
 			#contours,hierarchy = cv2.findContours(thresh, 3, 2)
 			#nocolor = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
-			ret, thresh = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY)
-			_, contours, hierarchy = cv2.findContours(thresh, 2, 1)
-
-			# We only care about the BIGGEST contour here
-			c = max(contours, key = cv2.contourArea)
-			# We treat it as an ellipse to account for irregularities in shape.
-			ellipse = cv2.fitEllipse(c)
-
-			# This makes an 'image' of all nothing with the same size as the original
-			mask = np.zeros(frame.shape, dtype=np.uint8)
-			# This draws the ellipse onto the empty shape we just made.
-			# The parameters are from the fitEllipse, but they have to be INT.
-			cv2.ellipse(mask, (int(ellipse[0][0]), int(ellipse[0][1])), (int(ellipse[1][0]), int(ellipse[1][1])), int(ellipse[2]), 0, 360, (255, 255, 255), -1, 8)
-			result = frame & mask
-
-			# Now the mask is shifted such that the center of the ellipse is in the center of the screen.
-			xdi = (col / 2) - int(ellipse[0][0])
-			ydi = (row / 2) - int(ellipse[0][1])
-			M = np.float32([[1, 0, xdi], [0, 1, ydi]])
-			result = cv2.warpAffine(result, M, (col, row))
-
-			# Subtract background from stilled frame
-			# learningRate = 0.05 deals with moon features better, but may be too taxing for Pi
-			result = fgbg.apply(result, learningRate=.1)
-			#result = cv2.GaussianBlur(result, (3, 3), 0)
-			#can = cv2.Canny(result, 5, 200)
-			#result = cv2.addWeighted(result,0.5,can,0.5,0)
-			result = cv2.morphologyEx(result, cv2.MORPH_GRADIENT, mat)
-
-			# Retest the image for contours
-			#gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-			ret, thresh = cv2.threshold(result, 127, 255, cv2.THRESH_BINARY)
-			_, contours, hierarchy = cv2.findContours(thresh, 2, 1)
 
 			##This if is required to ignore initial frames
 			#if not contours:
@@ -116,7 +160,7 @@ try:
 						##else the pixel is in the ellipse.  Therefore we are going to do things to it.
 						#else:
 							##adjust the pixel to a new coordinate with respect to the center of moment
-							## 
+							##
 							## This is a new thing I'm not so sure of to rotate the array
 							## ndi.rotate(input, angle)
 							## I need to see if this rotates from the center of mass of the image.  It may
@@ -135,7 +179,7 @@ try:
 
 				## Initialize empty list
 				##lst_intensities = []
-				
+
 				### For each list of contour points...
 				##for i in range(len(cnt)):
 					### Create a mask image that contains the contour filled in
@@ -173,28 +217,6 @@ try:
 						####radius = int(radius)
 						####cv2.circle(frame,center,radius,(0,0,255),2)
 
-			cv2.imwrite('current.png', frame)
-			# This mess displays different things side by side
-			# The Mother window shows the manipulated image that is being used for cakculations
-			# The Original window shows the contours overlaid on the original frame
-			cv2.namedWindow("Mother", cv2.WINDOW_NORMAL)
-			cv2.resizeWindow("Mother", 700, 500)
-			cv2.imshow("Mother", result)
-			cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
-			cv2.resizeWindow("Original", 700, 500)
-			cv2.imshow("Original", frame)
-		else:
-			# The next frame is not ready, so we try to read it again
-			cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
-			print "frame is not ready"
-			# It is better to wait for a while for the next frame to be ready
-			cv2.waitKey(1000)
 
-		if cv2.waitKey(10) & 0xFF == ord('q'):
-			break
-
-	cap.release()
-	cv2.destroyAllWindows()
-except KeyboardInterrupt:
-	cap.release()
-	cv2.destroyAllWindows()
+if __name__ == '__main__':
+	main()
