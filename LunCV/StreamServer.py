095e9d3ab520ca04e4cf542fbe2c60c46232f73a
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2018-07-19 14:18:59 -0500

    upgraded Stream Server with recv

--- LunCV/StreamServer.py
+++ LunCV/StreamServer.py
@@ -7,6 +7,8 @@
 import fcntl
 import struct
 import socket
+#import sys
+import time
 #from io import BytesIO
 #from threading import *
 import pygame
@@ -33,13 +35,169 @@ def server():
 	servsock.listen(5)
 	print("\nServer started at " + str(ip_address) + " at port " + str(port))
 	pygame.camera.init()
+
 	while True:
-		img = Image.open('tmp.png')
-		img = img.resize(img)
-		imgbyte = img.tobytes()
-		#len for 640x480 bytes is 921600
+		servsock.listen(5)
 		client_sock, _ = servsock.accept()
-		client_sock.sendall(imgbyte)
+		data = client_sock.recv(20)
+		if not data:
+			break
+		if data == "a":
+			img = Image.open('tmp.png')
+			img = img.resize(img)
+			imgbyte = img.tobytes()
+			#len for 640x480 bytes is 921600
+			client_sock.sendall(imgbyte)
+			break
+		if data == "hats":
+			break
+
+###################################################################################################
+
+def recv_robust(servsock, timeout):
+	'''A robust recv method which amalgamates timeout, and end of message checks.
+	It cannot do a message length check.
+	'''
+
+	# Empty variables
+	#total_len = 0
+	total_data = []
+	data = ''
+	#size = sys.maxint
+	#size_data = sock_data = ''
+
+	# This is our end of message character.  Make sure it is a single character, so won't break up
+	endpoint = '\0'
+
+	# Declare that we are considering timeouts.
+	servsock.setblocking(False)
+
+	# Start counting the time
+	begin = time.time()
+
+	while True:
+		#if you got some data, then break after wait sec
+		if total_data and time.time()-begin > timeout:
+			break
+		#if you got no data at all, wait a little longer
+		elif time.time()-begin > timeout*2:
+			break
+		try:
+			data = servsock.recv(8192)
+			if data:
+				if endpoint in data:
+					total_data.append(data[:data.find(endpoint)])
+					break
+				total_data.append(data)
+				begin = time.time()
+				if len(total_data) > 1:
+					#check if end_of_data was split
+					last_pair = total_data[-2]+total_data[-1]
+					if endpoint in last_pair:
+						total_data[-2] = last_pair[:last_pair.find(endpoint)]
+						total_data.pop()
+						break
+					else:
+						time.sleep(0.1)
+		except socket.error:
+			pass
+	return ''.join(total_data)
+
+
+
+	#while True:
+		#data = servsock.recv(8192)
+		#if not data:
+			#break
+		#total_data.append(data)
+	#return ''.join(total_data)
+
+#def recv_timeout(servsock, timeout=2):
+	#'''Let's us know if we time out during TCP
+	#'''
+
+	## Declare that we are considering timeouts.
+	#servsock.setblocking(False)
+
+	## Empty variables
+	#total_data = []
+	#data = ''
+
+	## Start counting the time
+	#begin = time.time()
+
+	#while 1:
+		##if you got some data, then break after wait sec
+		#if total_data and time.time()-begin > timeout:
+			#break
+		##if you got no data at all, wait a little longer
+		#elif time.time()-begin > timeout*2:
+			#break
+		#try:
+			#data = servsock.recv(8192)
+			#if data:
+				#total_data.append(data)
+				#begin = time.time()
+			#else:
+				#time.sleep(0.1)
+		#except socket.error:
+			#pass
+	#return ''.join(total_data)
+
+
+
+#def recv_end(servsock):
+	#'''Check for the end of the received message
+	#'''
+
+	## Empty variables
+	#total_data = []
+	#data = ''
+
+	#while True:
+		#data = servsock.recv(8192)
+		#if endpoint in data:
+			#total_data.append(data[:data.find(endpoint)])
+			#break
+		#total_data.append(data)
+		#if len(total_data) > 1:
+			##check if end_of_data was split
+			#last_pair = total_data[-2]+total_data[-1]
+			#if endpoint in last_pair:
+				#total_data[-2] = last_pair[:last_pair.find(endpoint)]
+				#total_data.pop()
+				#break
+	#return ''.join(total_data)
+
+#def recv_size(servsock):
+	#'''Determines the size of the packet which is being sent
+	#data length is packed into 4 bytes
+	#'''
+
+	## Initialize empty variables
+	#total_len = 0
+	#total_data = []
+	#size = sys.maxint
+	#size_data = sock_data = ''
+
+	## Recv packet size
+	#recv_size = 8192
+	#while total_len < size:
+		#sock_data = servsock.recv(recv_size)
+		#if not total_data:
+			#if len(sock_data) > 4 or len(size_data) > 4:
+				#size_data += sock_data
+				#size = struct.unpack('>i', size_data[:4])[0]
+				#recv_size = size
+				#if recv_size > 524288:
+					#recv_size = 524288
+				#total_data.append(size_data[4:])
+			#else:
+				#size_data += sock_data
+		#else:
+			#total_data.append(sock_data)
+		#total_len = sum([len(i) for i in total_data])
+	#return ''.join(total_data)
 
 server()
 
