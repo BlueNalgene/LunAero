d2525093ee974c3bb4de626d902555c57b297249
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2019-05-15 17:25:16 -0500

    fix root user error

--- ODROID_INSTALL.sh
+++ ODROID_INSTALL.sh
@@ -1,12 +1,12 @@
 sudo apt update
 sudo apt -y install git python3-pip python3-setuptools mercurial python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev libfreetype6-dev libconfuse-dev libboost-all-dev libusb-1.0-0-dev doxygen swig python3-opencv v4l-utils
-cd /home/$USER/Documents
+cd /home/odroid/Documents
 hg clone https://bitbucket.org/pygame/pygame
 cd pygame
 python3 setup.py build
 sudo python3 setup.py install
 sudo -H pip3 install Adafruit-GPIO
-cd /home/$USER/Documents
+cd /home/odroid/Documents
 wget https://www.intra2net.com/en/developer/libftdi/download/libftdi1-1.3.tar.bz2
 tar xvf libftdi1-1.3.tar.bz2
 cd libftdi1-1.3
@@ -20,12 +20,12 @@ sudo -H pip3 install Adafruit-PCA9685
 sudo mv /usr/local/lib/python3.6/dist-packages/Adafruit-GPIO/FT232H.py /usr/local/lib/python3.6/dist-packages/Adafruit-GPIO/FT232H.py.old
 sudo rm /usr/local/lib/python3.6/dist-packages/Adafruit-GPIO/FT232H.py
 sudo curl https://gist.githubusercontent.com/BlueNalgene/f2324cbb7bd9b3060b46ebcaaf7d8a15/raw/4a5fdfc4f0a064d2b7a8a75724b49a78662af108/gistfile1.txt > FT232H.py
-cd /home/$USER/Documents
+cd /home/odroid/Documents
 git clone --recursive https://github.com/hardkernel/WiringPi2-Python
 cd WiringPi2-Python
 sudo python3 setup.py
 cd ../
-sudo rm -r /home/$USER/Documents/libftdi1-1.3
-sudo rm -r /home/$USER/Documents/pygame
-sudo rm -r /home/$USER/Documnets/WiringPi2-Python
-rm /home/$USER/Documents/libftdi1-1.3.tar.bz2
+sudo rm -r /home/odroid/Documents/libftdi1-1.3
+sudo rm -r /home/odroid/Documents/pygame
+sudo rm -r /home/odroid/Documnets/WiringPi2-Python
+rm /home/odroid/Documents/libftdi1-1.3.tar.bz2
